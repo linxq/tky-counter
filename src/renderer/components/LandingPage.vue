be8d22ac168603345fd10a80e55b539968092826
linxqb <linxqb@126.com>
2020-01-18 15:19:39 +0800

    upgrade

--- src/renderer/components/LandingPage.vue
+++ src/renderer/components/LandingPage.vue
@@ -1,36 +1,8 @@
 <template>
-  <div id="wrapper">
-    <img id="logo" src="~@/assets/logo.png" alt="electron-vue" />
-    <main>
-      <div class="left-side">
-        <span class="title">Welcome to your new project!</span>
-        <system-information></system-information>
-      </div>
-
-      <div class="right-side">
-        <div class="doc">
-          <div class="title">Getting Started</div>
-          <p>
-            electron-vue comes packed with detailed documentation that covers everything from
-            internal configurations, using the project structure, building your application,
-            and so much more.
-          </p>
-          <button
-            @click="open('https://simulatedgreg.gitbooks.io/electron-vue/content/')"
-          >Read the Docs</button>
-          <br />
-          <br />
-        </div>
-        <div class="doc">
-          <div class="title alt">Other Documentation</div>
-          <button class="alt" @click="open('https://electron.atom.io/docs/')">Electron</button>
-          <button class="alt" @click="open('https://vuejs.org/v2/guide/')">Vue.js</button>
-          <el-button>
-            <router-link to="/base">base</router-link>
-          </el-button>
-        </div>
-      </div>
-    </main>
+  <div id="wrapper" style="margin:auto;text-align: center;">
+    <router-link to="/base" style="text-decoration: none"
+      ><h1>欢迎使用，点击进入</h1></router-link
+    >
   </div>
 </template>
 
